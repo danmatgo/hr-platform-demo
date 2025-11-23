e91cfece513541cd880ae4432b08fe433a447999
Daniel Matapi <danmatgo@gmail.com>
2025-11-23 11:24:54 -0500

    routes: set root to Devise sessions within devise_scope :user; keep authenticated root to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     root 'dashboard#index', as: :authenticated_root
   end
 
-  unauthenticated :user do
-    root 'devise/sessions#new', as: :unauthenticated_root
+  devise_scope :user do
+    root to: 'devise/sessions#new'
   end
 
   get 'dashboard/index'
